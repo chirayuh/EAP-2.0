76d6c11dff6fa57eb512050d8c97e6c0b1afabb7
Chirayu H <31740250+chirayuh@users.noreply.github.com>
2019-09-21 00:16:40 +0530

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -3,4 +3,9 @@ app.controller("myCtrl", function($scope) {
   $scope.firstName = "John";
   $scope.lastName = "Doe";
 });
-
+document.addEventListener('DOMContentLoaded', function() {
+  var select = document.querySelectorAll('select');
+  M.FormSelect.init(select, {});
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {});
+});
