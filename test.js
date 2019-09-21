8263269e35974c667a21a5a7325ae53adc4816bb
full meals <chirayuh69@gmail.com>
2019-09-21 11:21:38 +0530

    Changed the button to default color

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     M.FormSelect.init(elems, {});
+    var actionbtn = document.querySelectorAll('.fixed-action-btn');
+    M.FloatingActionButton.init(actionbtn, {});
 });
