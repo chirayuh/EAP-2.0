675dd9b94934b908b6827a4eac449afd7f26d61b
full meals <chirayuh69@gmail.com>
2019-09-28 22:24:36 +0530

    Testing how to open file using javascript

--- test.js
+++ test.js
@@ -4,3 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var actionbtn = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(actionbtn, {});
 });
+
+const input = document.querySelector('input[type="file"]')
+input.addEventListener('change', function (e) {
+    console.log(input.files);
+    console.log("Count of files: "+input.files.length);
+    const reader = new FileReader();
+    reader.onload = function () {
+        console.log(reader.result);
+        // const img = new Image()
+        // img.src = reader.result
+        // document.body.appendChild(reader.result)
+    }
+    console.log(reader.readAsText(input.files[0]));
+}, false);
+
+// inp.onchange = e => {
+//     const reader = new FileReader();
+//     reader.onload = e => {
+//       var myObj = {
+//         name: inp.files[0].name,
+//         data: reader.result.split('base64,')[1]
+//       };
+//       console.log(JSON.stringify(myObj));
+//     };
+//     reader.readAsDataURL(inp.files[0]);
+//   };
